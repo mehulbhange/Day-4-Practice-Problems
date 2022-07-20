76ffdbdab8f22a466841ab8054556da7a009c09e
mehulbhange <bhangemehul@gmail.com>
2022-07-20 10:09:14 +0530

    [Mehul]:added isEmployeePresent

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,5 +2,21 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
         
+        Employee employee = new Employee();
+        employee.isEmployeePresent();
+    }
+}
+
+class Employee{
+
+    public void isEmployeePresent(){
+        int isPresent = 1;
+        int empCheck = (int) Math.floor(Math.random()*10)%2;
+
+        if(empCheck == isPresent){
+            System.out.println("Employee is present");
+        }else{
+            System.out.println("Employee is not present");
+        }
     }
 }
