c0e6608aac578cd2d0d441eabe309f5bcc51e1d0
mehulbhange <bhangemehul@gmail.com>
2022-07-20 16:29:41 +0530

    [Mehul]:added snake and ladder game to SnakeAndLadderUC1

---
+++ SnakeAndLadder.java
@@ -0,0 +1,10 @@
+public class SnakeAndLadder{
+
+    public static void main(String[] args) {
+        System.out.println("Snake and ladder game. Player1 at 0");
+        
+        int player1 = 0;
+
+    }
+    
+}
